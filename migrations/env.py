77c3773335b4cd9297ca14f704dd0890f0b2444a
mikefromit <mike.arbelaez@gmail.com>
2017-11-11 21:57:41 -0600

    added alembic and initial schema

---
+++ migrations/env.py
@@ -0,0 +1,83 @@
+from __future__ import with_statement
+
+import os
+import sys
+from logging.config import fileConfig
+from os.path import dirname, abspath
+
+from alembic import context
+from sqlalchemy import create_engine
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+sys.path.insert(1, os.path.join(os.getcwd(), 'website'))
+
+from website import create_app
+from website.core import db
+from website.utils import make_database_url
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+app = create_app()
+
+target_metadata = db.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+url = make_database_url()
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_engine(url)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
